b1fccd4b928a9f9a9065b6da00ddd292d0ec397c
Clément VALENTIN <clement.valentin@clmdm.net>
2014-10-23 21:50:01 +0200

    GROS ZEUB

--- tp0/src/tp0/RangeSliderListener.java
+++
@@ -1,9 +0,0 @@
-package tp0;
-
-import java.util.EventListener;
-
-public interface RangeSliderListener extends EventListener {
-	void rangeSliderChanged(RangeSliderModel model);
-	void minBoundChanged(int oldValue, int newValue);
-	void maxBoundChanged(int oldValue, int newValue);
-}
